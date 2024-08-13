0a41b0b473da9ad7200cf60b163c4b7abc02332b
arabidopsis <ian.castleden@gmail.com>
2024-08-14 01:10:13 +0800

    catch errors

--- src/clean.jl
+++ src/clean.jl
@@ -6,19 +6,23 @@ function clean(directory::String, wait::Real; old=2.0, verbose=false)
     @info "checking $directory: wait=$(wait)secs old=$(old)days"
     while true
         now = time()
-        for (root, dirs, files) in walkdir(directory)
-            for file in files
-                f = joinpath(root, file)
-                t = mtime(f)
-                days = (now - t) / (60 * 60 * 24)
-                if days > old
-                    rm(f, force=true)
-                    if verbose
-                        @info "removed: $f"
+        try
+            for (root, dirs, files) in walkdir(directory)
+                for file in files
+                    f = joinpath(root, file)
+                    t = mtime(f)
+                    days = (now - t) / (60 * 60 * 24)
+                    if days > old
+                        rm(f, force=true)
+                        if verbose
+                            @info "removed: $f"
+                        end
                     end
-                end
 
+                end
             end
+        catch e
+            @error "walkdir: $(e)"
         end
         sleep(wait)
     end
