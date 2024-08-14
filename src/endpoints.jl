123febe1add94fe983c2fef62f4418137d7eaa06
arabidopsis <ian.castleden@gmail.com>
2024-08-14 10:57:35 +0800

    species

--- src/endpoints.jl
+++ src/endpoints.jl
@@ -5,19 +5,20 @@ import Emma: emmaone, writeGFF, TempFile, drawgenome, rotate, writeGB
 
 
 function make_task(directory::String=".")
-    function task_emma(; fasta::String="", svg::String="no", rotate_to::String="", gb="no")
+    function task_emma(; fasta::String="", svg::String="no", rotate_to::String="", gb="no",
+        species::String="vertebrate")
         if !isfile(fasta)
             error("no such file: $(fasta)")
         end
-        translation_table = 2
+        translation_table = species == "vertebrate" ? 2 : 5
         tempfile = TempFile(directory)
-        id, gffs, genome = fetch(@spawnat :any emmaone(tempfile, fasta, 2))
+        id, gffs, genome = fetch(@spawnat :any emmaone(tempfile, fasta, translation_table))
         offset = 0
         if rotate_to != ""
             gffs, genome, offset = rotate(rotate_to, gffs, genome)
         end
         ret = Dict("uuid" => tempfile.uuid, "gffs" => gffs, "id" => id,
-            "length" => length(genome), "offset" => offset)
+            "length" => length(genome), "offset" => offset, "species" => species)
         if svg == "yes"
             # mRNAless = filter(x -> x.ftype != "mRNA" && x.ftype != "CDS", gffs)
             ret["svg"] = drawgenome(id, length(genome), gffs)
