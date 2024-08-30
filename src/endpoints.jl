cddbf06d744680c30e011608c2ca52e3ade0a03d
arabidopsis <ian.castleden@gmail.com>
2024-08-30 20:15:59 +0800

    send fasta body

--- src/endpoints.jl
+++ src/endpoints.jl
@@ -4,6 +4,7 @@ import Distributed: @spawnat
 import Emma: emmaone, writeGFF, TempFile, drawgenome, rotate, writeGB
 import CodecZlib: GzipDecompressorStream
 using FASTX
+import Base64: base64decode
 
 @kwdef struct CmdArgs
     fasta::String = ""
@@ -11,6 +12,7 @@ using FASTX
     rotate_to::String = ""
     gb::String = "no"
     species::String = "vertebrate"
+    is_file::Bool = true
 end
 
 function maybe_gzread(f::Function, filename::String)
@@ -21,10 +23,17 @@ function maybe_gzread(f::Function, filename::String)
     end
 end
 
-function emmatwo(tempfile::TempFile, infile::String, translation_table::Integer)
+function emmatwo(tempfile::TempFile, infile::String, translation_table::Integer; is_file=true)
     target = FASTA.Record()
-    maybe_gzread(infile) do io
-        FASTA.Reader(io) do reader
+    if is_file
+        maybe_gzread(infile) do io
+            FASTA.Reader(io) do reader
+                read!(reader, target)
+            end
+        end
+    else
+        fasta = String(base64decode(infile))
+        FASTA.Reader(IOBuffer(fasta)) do reader
             read!(reader, target)
         end
     end
@@ -70,13 +79,16 @@ function emmathree(tempdirectory::String, args::CmdArgs; use_threads::Bool=false
 end
 
 function emmafour(tempdirectory::String, args::CmdArgs)
-    fasta = expanduser(args.fasta)
-    if !isfile(fasta)
-        error("no such file: $(fasta)")
+    fasta = args.fasta
+    if args.is_file
+        fasta = expanduser(fasta)
+        if !isfile(fasta)
+            error("no such file: $(fasta)")
+        end
     end
     translation_table = args.species == "vertebrate" ? 2 : 5
     tempfile = TempFile(tempdirectory)
-    id, gffs, genome, logs = emmatwo(tempfile, args.fasta, translation_table)
+    id, gffs, genome, logs = emmatwo(tempfile, fasta, translation_table; is_file=args.is_file)
 
     offset = 0
     if args.rotate_to != ""
@@ -123,9 +135,11 @@ function make_task4(tempdirectory::String=".", use_threads::Bool=false)
         svg::String="no",
         rotate_to::String="",
         gb::String="no",
-        species::String="vertebrate"
+        species::String="vertebrate",
+        is_file::String="true"
     )
-        args = CmdArgs(; fasta=fasta, svg=svg, rotate_to=rotate_to, gb=gb, species=species)
+        args = CmdArgs(; fasta=fasta, svg=svg, rotate_to=rotate_to, gb=gb, species=species, 
+            is_file=startswith(is_file, r"1|t|T"))
         if use_threads
             fetch(Threads.@spawn emmafour(tempdirectory, args))
 
