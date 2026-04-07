dd96b7284a0871d150c9804280402d81ea6d96d4
Ian Castleden <ian.castleden@uwa.edu.au>
2026-04-08 05:11:49 +0800

    update juliaWebAPI

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 exec julia --startup-file=no --project=. --threads=4 \
-    srvr.jl --console --use-threads \
+    srvr.jl --console --use-threads --level=info \
     --sleep-hours=1 --watch=~/github/emma-website/instance/datadir
