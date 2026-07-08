0968bb6694b85531495c6e11598c598aac293a5b
Ian Castleden <ian.castleden@uwa.edu.au>
2026-07-08 16:31:09 +0800

    concurrency test

---
+++ runall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+fastas=(~/Sites/websites/chloe/kelly/NC_002762.1.fa
+        ~/Sites/websites/chloe/kelly/NC_005086.1.fa
+        ~/Sites/websites/chloe/kelly/NC_006050.1.fa
+        ~/Sites/websites/chloe/kelly/NC_006290.1.fa)
+for fasta in "${fastas[@]}"
+do
+    echo $fasta
+    n=$(basename $fasta)
+    curl --silent "http://127.0.0.1:9998/chloe2_json?fasta=${fasta}" > res$n.json &
+done
