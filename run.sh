4a0d50748496fe5e0794af64017c100b2dd584eb
Ian Castleden <ian.castleden@uwa.edu.au>
2026-07-09 00:42:23 +0800

    write to junk

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 exec julia --startup-file=no --project=. --threads=4 \
     srvr.jl --console --level=info --workers=4 \
+    --use-threads \
     --sleep-hours=1 --watch=~/github/emma-website/instance/datadir
--- runall.sh
+++ runall.sh
@@ -3,9 +3,10 @@ fastas=(~/Sites/websites/chloe/kelly/NC_002762.1.fa
         ~/Sites/websites/chloe/kelly/NC_005086.1.fa
         ~/Sites/websites/chloe/kelly/NC_006050.1.fa
         ~/Sites/websites/chloe/kelly/NC_006290.1.fa)
+mkdir -p junk
 for fasta in "${fastas[@]}"
 do
     echo $fasta
     n=$(basename $fasta)
-    curl --silent "http://127.0.0.1:9998/chloe2_json?fasta=${fasta}" > res$n.json &
+    curl --silent "http://127.0.0.1:9998/chloe2_json?fasta=${fasta}" > junk/res$n.json &
 done
